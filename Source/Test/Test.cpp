44b88142653041f3a4bb3eb3faab66b45c4febae
Adam Sawicki <adam@asawicki.info>
2015-11-11 21:35:01 +0100

    Started using Google Test library for unit testing.

--- Source/Test/Test.cpp
+++ Source/Test/Test.cpp
@@ -1,52 +1,185 @@
 #include <RegScript2.hpp>
+#include <memory>
 #include <cstddef>
+#include <gtest/gtest.h>
 
 #ifdef _DEBUG
 #pragma comment(lib, "CommonLib_Debug.lib")
+#pragma comment(lib, "gtestd.lib")
 #else
 #pragma comment(lib, "CommonLib_Release.lib")
+#pragma comment(lib, "gtest.lib")
 #endif
 
 namespace rs2 = RegScript2;
 
-class NestedClass1
+class Environment : public ::testing::Environment
 {
 public:
-	rs2::FloatParam ParamA;
+	virtual void SetUp();
+	virtual void TearDown();
 };
 
-class MyClass1
+void Environment::SetUp()
+{
+	common::GameTime::Initialize();
+}
+
+void Environment::TearDown()
+{
+}
+
+class SimpleClass
 {
 public:
-	rs2::BoolParam Param1;
-	rs2::FloatParam Param2;
+	rs2::BoolParam BoolParam;
+	rs2::UintParam UintParam;
+	rs2::FloatParam FloatParam;
+	rs2::GameTimeParam GameTimeParam;
+
+	static std::unique_ptr<rs2::ClassDesc> CreateClassDesc();
 };
 
-class MyClass2 : public MyClass1
+std::unique_ptr<rs2::ClassDesc> SimpleClass::CreateClassDesc()
+{
+	std::unique_ptr<rs2::ClassDesc> classDesc =
+		std::make_unique<rs2::ClassDesc>(L"SimpleClass", sizeof(SimpleClass));
+
+	classDesc->AddParam(
+		L"BoolParam",
+		offsetof(SimpleClass, BoolParam),
+		rs2::BoolParamDesc().SetDefault(true));
+	classDesc->AddParam(
+		L"UintParam",
+		offsetof(SimpleClass, UintParam),
+		rs2::UintParamDesc().SetDefault(123));
+	classDesc->AddParam(
+		L"FloatParam",
+		offsetof(SimpleClass, FloatParam),
+		rs2::FloatParamDesc().SetDefault(3.14f));
+	classDesc->AddParam(
+		L"GameTimeParam",
+		offsetof(SimpleClass, GameTimeParam),
+		rs2::GameTimeParamDesc().SetDefault(common::MillisecondsToGameTime(1023)));
+
+	return classDesc;
+}
+
+class SimpleFixture : public ::testing::Test
+{
+protected:
+	std::unique_ptr<rs2::ClassDesc> m_SimpleClassDesc;
+
+	SimpleFixture();
+	~SimpleFixture() { }
+	virtual void SetUp() { }
+	virtual void TearDown() { }
+};
+
+SimpleFixture::SimpleFixture() :
+	m_SimpleClassDesc(SimpleClass::CreateClassDesc())
+{
+}
+
+TEST_F(SimpleFixture, SetDefault)
+{
+	SimpleClass obj;
+	m_SimpleClassDesc->SetObjToDefault(&obj);
+
+	EXPECT_EQ(true, obj.BoolParam.Value);
+	EXPECT_EQ(123, obj.UintParam.Value);
+	EXPECT_EQ(3.14f, obj.FloatParam.Value);
+	EXPECT_EQ(common::MillisecondsToGameTime(1023), obj.GameTimeParam.Value);
+}
+
+TEST_F(SimpleFixture, CopyObj)
+{
+	SimpleClass obj1, obj2;
+	m_SimpleClassDesc->SetObjToDefault(&obj1);
+	m_SimpleClassDesc->CopyObj(&obj2, &obj1);
+
+	EXPECT_EQ(true, obj2.BoolParam.Value);
+	EXPECT_EQ(123, obj2.UintParam.Value);
+	EXPECT_EQ(3.14f, obj2.FloatParam.Value);
+	EXPECT_EQ(common::MillisecondsToGameTime(1023), obj2.GameTimeParam.Value);
+}
+
+class ContainerClass
 {
 public:
-	rs2::FloatParam Param3;
-	rs2::ClassParam<NestedClass1> NestedClassParam;
-	rs2::FixedSizeArrayParam<NestedClass1, 3> NestedArray;
+	rs2::ClassParam<SimpleClass> ClassParam;
+	rs2::FixedSizeArrayParam<rs2::UintParam, 3> FixedSizeArrayParam;
+
+	static std::unique_ptr<rs2::ClassDesc> CreateClassDesc(const rs2::ClassDesc* simpleClassDesc);
 };
 
-int main()
+std::unique_ptr<rs2::ClassDesc> ContainerClass::CreateClassDesc(const rs2::ClassDesc* simpleClassDesc)
 {
-	rs2::ClassDesc myNestedClassDesc(L"NestedClass1", sizeof(NestedClass1));
-	myNestedClassDesc.AddParam(L"ParamA", offsetof(NestedClass1, ParamA), rs2::FloatParamDesc().SetDefault(10.f));
+	std::unique_ptr<rs2::ClassDesc> classDesc =
+		std::make_unique<rs2::ClassDesc>(L"ContainerClass", sizeof(ContainerClass));
 
-	rs2::ClassDesc myClass1Desc(L"MyClass1", sizeof(MyClass1));
-	myClass1Desc.AddParam(L"Param1", offsetof(MyClass1, Param1), rs2::BoolParamDesc().SetDefault(true));
-	myClass1Desc.AddParam(L"Param2", offsetof(MyClass1, Param2), rs2::FloatParamDesc().SetDefault(2.f));
+	classDesc->AddParam(
+		L"ClassParam",
+		offsetof(ContainerClass, ClassParam),
+		rs2::ClassParamDesc(simpleClassDesc));
+	classDesc->AddParam(
+		L"FixedSizeArrayParam",
+		offsetof(ContainerClass, FixedSizeArrayParam),
+		rs2::FixedSizeArrayParamDesc(new rs2::UintParamDesc(rs2::UintParamDesc().SetDefault(124)), 3));
 
-	rs2::ClassDesc myClass2Desc(L"MyClass2", sizeof(MyClass2), &myClass1Desc);
-	myClass2Desc.AddParam(L"Param3", offsetof(MyClass2, Param3), rs2::FloatParamDesc().SetDefault(3.f));
-	myClass2Desc.AddParam(L"NestedClassParam", offsetof(MyClass2, NestedClassParam), rs2::ClassParamDesc(&myNestedClassDesc));
-	myClass2Desc.AddParam(L"NestedArray", offsetof(MyClass2, NestedArray), rs2::FixedSizeArrayParamDesc(new rs2::ClassParamDesc(&myNestedClassDesc), 3));
+	return classDesc;
+}
 
-	MyClass2 obj2;
-	myClass2Desc.SetObjToDefault(&obj2);
+class ContainerFixture : public ::testing::Test
+{
+protected:
+	std::unique_ptr<rs2::ClassDesc> m_SimpleClassDesc;
+	std::unique_ptr<rs2::ClassDesc> m_ContainerClassDesc;
 
-	MyClass2 obj3;
-	myClass2Desc.CopyObj(&obj3, &obj2);
+	ContainerFixture();
+	~ContainerFixture() { }
+	virtual void SetUp() { }
+	virtual void TearDown() { }
+};
+
+ContainerFixture::ContainerFixture() :
+	m_SimpleClassDesc(SimpleClass::CreateClassDesc()),
+	m_ContainerClassDesc(ContainerClass::CreateClassDesc(m_SimpleClassDesc.get()))
+{
+}
+
+TEST_F(ContainerFixture, SetDefault)
+{
+	ContainerClass obj;
+	m_ContainerClassDesc->SetObjToDefault(&obj);
+
+	EXPECT_EQ(true, obj.ClassParam.Value.BoolParam.Value);
+	EXPECT_EQ(123, obj.ClassParam.Value.UintParam.Value);
+	EXPECT_EQ(3.14f, obj.ClassParam.Value.FloatParam.Value);
+	EXPECT_EQ(common::MillisecondsToGameTime(1023), obj.ClassParam.Value.GameTimeParam.Value);
+	EXPECT_EQ(124, obj.FixedSizeArrayParam.Values[0].Value);
+	EXPECT_EQ(124, obj.FixedSizeArrayParam.Values[1].Value);
+	EXPECT_EQ(124, obj.FixedSizeArrayParam.Values[2].Value);
+}
+
+TEST_F(ContainerFixture, CopyObj)
+{
+	ContainerClass obj1, obj2;
+	m_ContainerClassDesc->SetObjToDefault(&obj1);
+	m_ContainerClassDesc->CopyObj(&obj2, &obj1);
+
+	EXPECT_EQ(true, obj2.ClassParam.Value.BoolParam.Value);
+	EXPECT_EQ(123, obj2.ClassParam.Value.UintParam.Value);
+	EXPECT_EQ(3.14f, obj2.ClassParam.Value.FloatParam.Value);
+	EXPECT_EQ(common::MillisecondsToGameTime(1023), obj2.ClassParam.Value.GameTimeParam.Value);
+	EXPECT_EQ(124, obj2.FixedSizeArrayParam.Values[0].Value);
+	EXPECT_EQ(124, obj2.FixedSizeArrayParam.Values[1].Value);
+	EXPECT_EQ(124, obj2.FixedSizeArrayParam.Values[2].Value);
+}
+
+int wmain(int argc, wchar_t** argv)
+{
+	::testing::AddGlobalTestEnvironment(new Environment());
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
